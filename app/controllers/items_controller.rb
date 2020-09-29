6e993c8cae8565df926f12196ae9d60501be6fc8
Yuta-Asano <70369660+Yuta-Asano@users.noreply.github.com>
2020-09-29 11:01:55 +0900

    Active hashおよびJavaScriptでの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,13 @@ class ItemsController < ApplicationController
   def new
     @items = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :information :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price, :information, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,45 @@
 class Item < ApplicationRecord
-  belong_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :area
+  belongs_to_active_hash :category
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :shipping_date
+  belongs_to_active_hash :status
+  belongs_to :user
   has_one_attached :image
+  
+  
+  validate :image_presence
+   def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:image, 'ファイルを添付してください')
+    end
+  end
 
   with_options presence: true do
     validates  :name           
-    validates  :price,                   
+    validates  :price, format: { with: /^[0-9]+$/, :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }     
     validates  :information  
+    validates  :area
+    validates  :category
+    validates  :delivery_fee
+    validates  :shipping_date
+    validates  :status
+  end
 
+    with_options numericality: { other_than: 1 } do
+      validates  :area_id
+      validates  :category_id
+      validates  :delivery_fee_id
+      validates  :shipping_date_id
+      validates  :status_id
+    end
+end
                   
 
 
-end
+
